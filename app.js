a7820ddc2e6095be7dfec54be47ca3cb7c9328c1
KIRAN M R <iamthekiranmr@gmail.com>
2023-03-04 20:43:13 +0530

    add and fetch products with mongoose

--- app.js
+++ app.js
@@ -1,42 +1,41 @@
-const path = require('path');
-const dotenv = require('dotenv')
-const express = require('express');
-const bodyParser = require('body-parser');
+const path = require("path");
+const dotenv = require("dotenv");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user')
+const errorController = require("./controllers/error");
 
+//const User = require("./models/user");
 
 const app = express();
-dotenv.config()
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+dotenv.config();
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req, res, next) => {
-  User.findById("63f458b0369131fb07e394cd")
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-  
-});
+// app.use((req, res, next) => {
+//   User.findById("63f458b0369131fb07e394cd")
+//     .then((user) => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch((err) => console.log(err));
+// });
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-
-mongoConnect(() => {
-  //console.log(client);
-  
+mongoose.connect(process.env.CONNECTION_URL).then((result)=>{
   app.listen(3000);
+  console.log('connected');
+}).catch(err =>{
+  console.log(err);
 })
--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,5 @@
 const Product = require("../models/product");
 
-
-
 exports.getAddProduct = (req, res, next) => {
   res.render("admin/edit-product", {
     pageTitle: "Add Product",
@@ -15,7 +13,12 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, price, description, imageUrl, null, req.user._id);
+  const product = new Product({
+    title: title,
+    price: price,
+    description: description,
+    imageUrl: imageUrl,
+  });
   product
     .save()
     .then((result) => {
@@ -90,8 +93,8 @@ exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   Product.deleteById(prodId)
     .then(() => {
-      console.log('DESTROYED PRODUCT');
-      res.redirect('/admin/products');
+      console.log("DESTROYED PRODUCT");
+      res.redirect("/admin/products");
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 };
