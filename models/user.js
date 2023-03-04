bb10a7694659043709677a66e6c5df1de587e2bd
KIRAN M R <iamthekiranmr@gmail.com>
2023-03-04 16:03:26 +0530

    order fuctionality completed

--- models/user.js
+++ models/user.js
@@ -73,7 +73,17 @@ class User {
 
   addOrder(){
     const db = getDb();
-    return db.collection('orders').insertOne(this.cart).then(result => {
+    return this.getCart().then((products)=>{
+      const order = {
+        items: products,
+        user: {
+          _id: new ObjectId(this._id),
+          name: this.name
+        }
+      }
+      return db.collection('orders').insertOne(order)
+    })
+    .then(result => {
       
       this.cart = {items: []}
       
@@ -84,6 +94,11 @@ class User {
         { $set: { cart: {items: []} } }
       );
     })
+  }
+
+  getOrders(){
+    const db = getDb();
+    return db.collection('orders').find({'user._id': new ObjectId(this._id)}).toArray();
   }
 
   static findById(userId) {
