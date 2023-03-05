e0ccb2064c39881cd983fab6ebd6e9b60730746e
KIRAN M R <iamthekiranmr@gmail.com>
2023-03-05 09:16:01 +0530

    cart added to database mongoose

--- controllers/admin.js
+++ controllers/admin.js
@@ -18,6 +18,7 @@ exports.postAddProduct = (req, res, next) => {
     price: price,
     description: description,
     imageUrl: imageUrl,
+    userId: req.user,
   });
   product
     .save()
@@ -78,7 +79,10 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.find()
+    //.select("title price -_id") // using select we can select which fields of product should be fetched.
+    //.populate("userId", "name") // populate is used to get the entire details of user rather than just the userId. name shows only include name
     .then((products) => {
+      console.log(products);
       res.render("admin/products", {
         prods: products,
         pageTitle: "Admin Products",
