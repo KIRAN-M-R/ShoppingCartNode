4442c334767f86d1dc32f628007fb237b46f496f
KIRAN M R <iamthekiranmr@gmail.com>
2023-03-05 10:41:06 +0530

    delete cart items mongoose

--- models/user.js
+++ models/user.js
@@ -35,6 +35,14 @@ userSchema.methods.addToCart = function (product) {
     return this.save()
 }
 
+userSchema.methods.removeFromCart = function (productId) {
+  const updatedCartItems = this.cart.items.filter(item => {
+        return item.productId.toString() !== productId.toString();
+      })
+      this.cart.items = updatedCartItems
+      return this.save()
+}
+
 module.exports = mongoose.model('User', userSchema);
 
 
@@ -98,9 +106,9 @@ module.exports = mongoose.model('User', userSchema);
 //   }
 
 //   deleteItemFromCart(productId){
-//     const updatedCartItems = this.cart.items.filter(item => {
+//const updatedCartItems = this.cart.items.filter(item => {
 //       return item.productId.toString() !== productId.toString();
-//     })
+//     })     
 //     const db = getDb();
 //     return db
 //       .collection("users")
