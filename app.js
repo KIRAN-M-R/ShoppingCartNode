47a967795813b00cdd6b478f94eec18d74f1a881
KIRAN M R <iamthekiranmr@gmail.com>
2023-03-08 19:49:40 +0530

    Session saved in database

--- app.js
+++ app.js
@@ -35,7 +35,10 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  User.findById("64037260661294670e02b962")
+  if(!req.session.user){
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then((user) => {
       req.user = user;
       next();
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const User = require('../models/user')
+
 exports.getLogin = (req, res, next) => {
   //const isLoggedIn = req.get('Cookie').trim().split('=')[1] === 'true'
  console.log(req.session.isLoggedIn);
@@ -11,6 +13,26 @@ exports.getLogin = (req, res, next) => {
 
 exports.postLogin = (req, res, next) => {
   //res.setHeader("Set-Cookie", "loggedIn=true");
-  req.session.isLoggedIn = true
-  res.redirect("/");
+  
+  
+  User.findById("64037260661294670e02b962")
+    .then((user) => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save((err)=>{ // usually req.session.save is not required but we use it here to make sure that redirection happens after the session is saved.
+        console.log(err);
+        res.redirect("/");
+      })
+      
+    })
+    .catch((err) => console.log(err));
+
+  
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy((err)=>{
+    console.log(err);
+    res.redirect('/')
+  })
 };
